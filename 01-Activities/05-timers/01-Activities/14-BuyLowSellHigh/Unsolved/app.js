79ded76bcb285fc2e50325c863ec5ce91dab7286
samlutz <sammlutz@gmail.com>
2019-09-07 15:55:16 -0400

    jquery revisit, built jquery to do from scratch

--- 01-Activities/05-timers/01-Activities/14-BuyLowSellHigh/Unsolved/app.js
+++ 01-Activities/05-timers/01-Activities/14-BuyLowSellHigh/Unsolved/app.js
@@ -5,10 +5,17 @@
 // Stock Prices
 var stockPrices = [1.32, 1.14, 1.45, 1.20, 1.34, 1.74, 1.18, 1.90, 1.1];
 
+const sortedPrices = stockPrices.sort(function(a,b) {
+    return a -b
+})
 
+console.log(sortedPrices)
 // Your Biggest Profit function
 var biggestProfit = function(stockArray, sharesBought) {
-
+    const profitMargin = stockPrices[stockPrices.length -1] - stockPrices[0];
+    console.log(profitMargin)
+    biggestProfit = profitMargin * sharesBought
+    console.log(Math.round(biggestProfit))
 };
 
 // A Call to your Biggest Profit function.
